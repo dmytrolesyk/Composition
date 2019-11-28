e8d26ac6c16325c9910f22409e65d005f1747fe4
dmytrolesyk <dmitriylesik@gmail.com>
2019-11-28 21:22:12 +0200

    working on labs on composition

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  if (fns.some(f => typeof f !== 'function')) {
+    throw new Error('Not a functional argument');
+  }
+  return x => fns.reduce((v, f) => f(v), x);
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,29 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const eventEmitter = {
+  on(event, handler) {
+    if (!this._eventHandlers) this._eventHandlers = {};
+    if (!this._eventHandlers[event]) this._eventHandlers[event] = [];
+    this._eventHandlers[event].push(handler);
+  },
+  emit(event, ...args) {
+    if (!this._eventHandlers || !this._eventHandlers[event]) return;
+    this._eventHandlers[event].forEach(handler => handler(...args));
+  }
+};
+
+const compose = (...fns) => {
+  const composed = par => {
+    let res;
+    try {
+      res = fns.reduceRight((v, f) => f(v), par);
+    } catch (error) {
+      composed.emit('error', error);
+      return;
+    }
+    return res;
+  };
+  return Object.assign(composed, eventEmitter);
+};
 
 module.exports = { compose };
